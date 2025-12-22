30d2ee8c2397387fe0a7ee35f985e52d5eb70e7e
rodmpissinati <rodrigomendespissinati@gmail.com>
2025-12-22 08:31:02 -0300

    fix: remove stray git commands from WeatherInfo.jsx

--- src/assets/components/WeatherInfo.jsx
+++ src/assets/components/WeatherInfo.jsx
@@ -13,8 +13,7 @@ function WeatherInfo(){
 
     return(
         <div className="flex h-50 flex-col items-center justify-center bg-gray-800  ">
-            <div className="w-100 bg-white/20 text-white placeholder-white/70 outline-none mx-auto text-center p-4 rounded-xl
-            border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">
+            <div className="w-100 bg-white/20 text-white placeholder-white/70 outline-none mx-auto text-center p-4 rounded-xl border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">
                 <h2 className="pb-3 text-2xl">{weatherData.name}</h2>
             
                 <div>
